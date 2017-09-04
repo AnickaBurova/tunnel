e0976c38abc30d1665f1caee3b454e46ec7ed9d9
Anicka Burova <anicka.burova@gmail.com>
2017-09-04 12:04:30 +0100

    0.1.0.0: Reading s3 using aws_sdk

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,15 @@
  * Last Modified Date: 04.09.2017
  * Last Modified By: Anicka Burova <anicka.burova@gmail.com>
  */
-extern crate s3;
+extern crate aws_sdk_rust;
 extern crate ini;
 
-use s3::bucket::Bucket;
-use s3::credentials::Credentials;
 use std::io::{self};
 
 
-fn load_credentials() -> io::Result<(Credentials,String)> {
+fn main() {
     use ini::Ini;
-    Ini::load_from_file("/home/milan/.s3cfg")
+    let _ = Ini::load_from_file("/home/milan/.s3cfg")
         .map_err(|err| {
             io::Error::new(io::ErrorKind::InvalidData, err)
         })
@@ -42,35 +40,64 @@ fn load_credentials() -> io::Result<(Credentials,String)> {
                 })
             .and_then(|(access_key, secret_key, region)| {
                 println!("{}\n{}\n{}", access_key, secret_key, region);
-                Ok((Credentials::new(&access_key, &secret_key, None), region.to_owned()))
+                use aws_sdk_rust::aws::common::credentials::{DefaultCredentialsProvider,ParametersProvider};
+                ParametersProvider::with_parameters(
+                    access_key.to_owned(),
+                    secret_key.to_owned(),
+                    None)
+                    .and_then(|credentials| {
+                        DefaultCredentialsProvider::new(Some(credentials))
+                    })
+                    .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+                    .and_then(|provider| {
+                        use std::str::FromStr;
+                        use aws_sdk_rust::aws::common::region::Region;
+                        Region::from_str(region)
+                            .and_then(|_region| {
+                                use aws_sdk_rust::aws::s3::endpoint::{Endpoint, Signature};
+                                //Ok(Endpoint::new(region, Signature::V4, None, None, None, None))
+                                Ok(Endpoint::new(Region::UsEast1, Signature::V4, None, None, None, None))
+                            })
+                            .and_then(|endpoint| {
+                                use aws_sdk_rust::aws::s3::s3client::S3Client;
+                                Ok(S3Client::new(provider, endpoint))
+                            })
+                            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+                    })
+            })
+            .and_then(|client| {
+                client.list_buckets()
+                    .and_then(|buckets| {
+                        println!("{:#?}", buckets);
+                        Ok(())
+                    })
+                    .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
             })
         })
-}
-
-fn main() {
-    let _ = load_credentials()
-        .and_then(|(credentials, region)| {
-            region
-                .parse()
-                .map_err(|_| {
-                    io::Error::new(io::ErrorKind::InvalidData, "Failed to parse region")
-                })
-                .and_then(|region| {
-                    Ok(Bucket::new("imsdistributionfiles", region, credentials))
-                })
-        })
-        .and_then(|bucket| {
-            bucket
-                .list("/exchange", None)
-                .and_then(|(list, code)| {
-                    println!("code = {}\nlist = {:?}", code, list);
-                    Ok(())
-                })
-                .map_err(|err| {
-                    println!("failed: {}", err.description());
-                    io::Error::new(io::ErrorKind::InvalidData, err.description().to_owned())
-                })
-        })
+    //let _ = load_credentials()
+        //.and_then(|(credentials, region)| {
+            //Ok(())
+            ////region
+                ////.parse()
+                ////.map_err(|_| {
+                    ////io::Error::new(io::ErrorKind::InvalidData, "Failed to parse region")
+                ////})
+                ////.and_then(|region| {
+                    ////Ok(Bucket::new("imsdistributionfiles", region, credentials))
+                ////})
+        //})
+        //.and_then(|bucket| {
+            //Ok(())
+            //bucket
+                //.list("/exchange", None)
+                //.and_then(|(list, code)| {
+                    //println!("code = {}\nlist = {:?}", code, list);
+                    //Ok(())
+                //})
+                //.map_err(|err| {
+                    //println!("failed: {}", err.description());
+                    //io::Error::new(io::ErrorKind::InvalidData, err.description().to_owned())
+                //})
+        //})
         .unwrap();
-    println!("Hello, world!");
 }
